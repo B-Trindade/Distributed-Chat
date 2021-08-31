cee7024e789ea4970a4e7b2f16af3c9e9b38f593
Breno Coll <collbreno@dcc.ufrj.br>
2021-08-31 02:32:12 -0300

    Removendo print de debug

--- client.py
+++ client.py
@@ -114,7 +114,6 @@ def send_messages(sock):
                 except:
                     print(f'Uso indevido do comando {CMD_CHAT}. Por favor digite o nome do usuário após o comando.')
                     continue
-                print('nao deia printar')
 
                 # Verifica se usuario escolheu um cliente diferente de si mesmo para iniciar um chat
                 if addressee == username:
